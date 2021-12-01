cd16c75f9c009c5e5388f0540c74d64963f32e59
Maxime PANNETIER <maxime.pannetier.mp@gmail.com>
2021-12-01 22:27:32 +0100

    gestion images + mdp + node module + env

--- backend/controller/users.js
+++ backend/controller/users.js
@@ -4,9 +4,33 @@ const Users = require('../models/users');
 
 const jwt = require('jsonwebtoken');
 
+const passwordValidator = require('password-validator');
+
 
 // signup = creation compte utilisateur
 exports.signup = (req, res, next) => {
+
+var schema = new passwordValidator();
+
+// Add properties to it
+schema
+.is().min(8)                                    // Minimum length 8
+.is().max(100)                                  // Maximum length 100
+.has().uppercase()                              // Must have uppercase letters
+.has().lowercase()                              // Must have lowercase letters
+.has().digits(2)                                // Must have at least 2 digits
+.has().not().spaces()                           // Should not have spaces
+.is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist these values
+
+// Validate against a password string
+if(!schema.validate(req.body.password))
+{
+  res.status(400).json({message: "le mot de passe n'est pas assez complexe !"});
+  return;
+}
+
+
+
   bcrypt.hash(req.body.password, 10) // hash mdp
     .then(hash => {
       const user = new Users({
